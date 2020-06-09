f9f34871a53120d0b3d717ef2d2b772458a820df
Emanuel Aguilar <Emanuel Aguilar@DESKTOP-M6GR8FE>
2020-06-09 15:38:54 -0600

    Amortizacion Base
    Se agrega parte de la division de la base para realizar la amortizacion

--- Emanuel Aguilar/models/Proyecto.js
+++ Emanuel Aguilar/models/Proyecto.js
@@ -5,4 +5,22 @@ const slug = require("slug");
 
 const shortid = require("shortid");
 
+const Proyecto = db.define(
+    "proyecto",
+    {
+      
+      monto: {
+        type: Sequilize.STRING,
+      },
+      awos: {
+        type: Sequilize.STRING,
+      },
+      tasa: {
+        type: Sequilize.STRING,
+      },
+      url: {
+        type: Sequilize.STRING,
+      },
+    }
+  );
 
